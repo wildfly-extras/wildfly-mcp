ee746c218d1c4d243b35ba8aa53a7b1ecbbcbc12
Stefano Maestri <stefano.maestri@javalinux.it>
2025-01-24 17:37:53 +0100

    adding a prompt

--- src/main/java/org/wildfly/mcp/WildFlyMCPServer.java
+++ src/main/java/org/wildfly/mcp/WildFlyMCPServer.java
@@ -5,6 +5,11 @@
 package org.wildfly.mcp;
 
 import com.sun.management.OperatingSystemMXBean;
+
+import io.quarkiverse.mcp.server.Prompt;
+import io.quarkiverse.mcp.server.PromptArg;
+import io.quarkiverse.mcp.server.PromptMessage;
+import io.quarkiverse.mcp.server.PromptResponse;
 import io.quarkiverse.mcp.server.TextContent;
 import java.util.List;
 
@@ -286,6 +291,15 @@ public class WildFlyMCPServer {
         }
     }
     
+    @Prompt(name = "Prometheus-metrics-chart", description = "Prometheus metrics chart")
+    PromptMessage prometheusMetricsChart() {
+        return PromptMessage.withUserRole(new TextContent("using available tools, get Prometheus metrics from wildfly server. " +
+            "You will repeat the invocation 3 times, being sure to wait 2 seconds between each invocation. " + 
+            "After all the 3 invocation has been completed you will organize the data in a table. " + 
+            "Then you will use this table to create a bar chart to visually compare the data. " + 
+            "Be sure to use at least 5 different data column and be sure to represent all data as bar in the chart"));
+    }
+
     @RegisterRestClient(baseUri = "http://foo:9990/metrics/")
     public interface WildFlyMetricsClient {
         
